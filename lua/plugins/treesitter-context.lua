9cd8d3c618251883505b3047fcd0b72b59f8a6ed
wangli <wangli858794774@gmail.com>
2026-07-17 19:06:57 +0800

    Reserve class and function name contexts at the very top
    Signed-off-by: wangli <wangli858794774@gmail.com>

---
+++ lua/plugins/treesitter-context.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter-context",
+    opts = {
+      max_lines = 4,
+      multiline_threshold = 2,
+      trim_scope = "outer",
+      mode = "cursor",
+      line_numbers = true,
+    },
+  },
+}
